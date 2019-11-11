e06c5a4c0c06ee8756ef23f3691840e8a8adff1a
zowarin <zowarin@gmail.com>
2019-11-11 15:14:24 -0600

    Tutotial: Django Completo

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def post_list(request):
-    return render(request , 'blog/post_list.html', {})
+    posts = Post.objects.all().order_by('published_date')
+    return render(request , 'blog/post_list.html', {'posts': posts}) 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
--- web_api_project/settings.py
+++ web_api_project/settings.py
@@ -121,5 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
